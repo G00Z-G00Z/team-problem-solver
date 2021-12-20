f97efb3173b02908cf306b4ce5a2488dc7138d97
Eduardo <edygomezgg4@gmail.com>
2021-12-20 11:55:04 -0600

    Override tailwind default theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import "../styles/App.css";
 
 function App() {
-	return (
-		<h1 className="text-2xl font-bold underline ">
-			Espero que estes bien y te mueras
-		</h1>
-	);
+	return <h1 className="text-9xl">Hola mundo estoy utilizando tailwind</h1>;
 }
 
 export default App;
