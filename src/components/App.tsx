393f0ae9afaaba75a9e390b102560ba2a45bf29c
Eduardo <edygomezgg4@gmail.com>
2021-12-23 10:29:09 -0600

    Fixed navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Base } from "./Base";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import { Home } from "./Home";
 
 export default function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Base />}></Route>
-			</Routes>
-		</Router>
+		<div className="">
+			{/* Nav bar */}
+			<nav className="w-full bg-CTA-300 fixed bottom-0 border-b-2 font-sans text-2xl">
+				<ul className="flex justify-evenly">
+					<Link to="/">Home</Link>
+					<Link to="/profile">Profile</Link>
+					<Link to="/help">Help</Link>
+					<Link to="/team">Teams</Link>
+				</ul>
+			</nav>
+			{/* Status */}
+			<section className="fixed top-0 w-full">
+				<div className="bg-CTA-100 flex justify-evenly">
+					No hay un equipo todavia
+				</div>
+			</section>
+			{/* Main content */}
+			<div className="p-8">
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</div>
 	);
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+	return (
+		<>
+			<h1 className="text-3xl font-serif">What's Your Problem ?</h1>
+		</>
+	);
+};
