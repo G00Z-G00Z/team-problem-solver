5dfba0f91e8530ae4cc5342b90bb8d43cb5f0038
Eduardo <edygomezgg4@gmail.com>
2021-12-29 11:33:26 -0600

    Added icons to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as HomeIcon } from "../assets/home.svg";
+import { ReactComponent as ProfileIcon } from "../assets/account.svg";
+import { ReactComponent as TeamIcon } from "../assets/team.svg";
+import { ReactComponent as HelpIcon } from "../assets/help.svg";
 
 export const NavBar = () => {
 	return (
-		<nav className="w-full bg-CTA-300 fixed bottom-0 border-b-2 font-sans text-2xl z-10">
+		<nav className="w-full bg-gray-200 fixed bottom-0 border-b-2 font-sans text-2xl z-10">
 			<ul className="flex justify-evenly">
-				<Link to="/">Home</Link>
-				<Link to="/profile">Profile</Link>
-				<Link to="/help">Help</Link>
-				<Link to="/team/select">Teams</Link>
+				<Link to="/" className="">
+					<HomeIcon className="fill-CTA-400" />
+				</Link>
+				<Link to="/profile">
+					<ProfileIcon className="fill-CTA-400" />
+				</Link>
+				<Link to="/help">
+					<HelpIcon className="fill-CTA-400" />
+				</Link>
+				<Link to="/team/select">
+					<TeamIcon className="fill-CTA-400" />
+				</Link>
 			</ul>
 		</nav>
 	);
