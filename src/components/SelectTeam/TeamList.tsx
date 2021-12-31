743826d8610005daf9a7a4462b74bdf7258d8751
G00Z_G00Z <edygomezgg4@gmail.com>
2021-12-31 12:56:04 -0600

    Made team list that filters teams

---
+++ src/components/SelectTeam/TeamList.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import { Team } from "../../types/interfaces";
+import { TeamDisplay } from "./TeamDisplay";
+
+interface Props {
+  nameQuery: string;
+  teamList: Team[];
+}
+
+export const TeamList: FC<Props> = ({ nameQuery, teamList }) => {
+  if (!nameQuery)
+    return (
+      <>
+        {teamList.map((t, idx) => (
+          <TeamDisplay team={t} key={idx} />
+        ))}
+      </>
+    );
+
+  const parsedQuery = nameQuery.toLocaleLowerCase() as string;
+
+  const filteredTeam = teamList.filter(({ name }) =>
+    name.toLowerCase().includes(parsedQuery)
+  );
+
+  if (filteredTeam.length === 0)
+    return (
+      <>
+        <h1>No se encontro ningun equipo :(</h1>
+      </>
+    );
+
+  return (
+    <>
+      {teamList.map((t, idx) =>
+        t.name.toLowerCase().startsWith(parsedQuery) ? (
+          <TeamDisplay team={t} key={idx} />
+        ) : null
+      )}
+    </>
+  );
+};
