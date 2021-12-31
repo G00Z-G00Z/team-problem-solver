941dd21b55ad7d560d068411e63325385444aabf
G00Z_G00Z <edygomezgg4@gmail.com>
2021-12-31 12:40:20 -0600

    Made search bar filter teams

--- src/components/SelectTeam/SelectTeamPage.tsx
+++ src/components/SelectTeam/SelectTeamPage.tsx
@@ -7,55 +7,59 @@ import { TeamDisplay } from "./TeamDisplay";
 import { ReactComponent as SearchLogo } from "../../assets/search.svg";
 
 interface Props {
-	teams: Team[];
+  teams: Team[];
 }
 
 export const SelectTeamPage: FC<Props> = React.memo(({ teams }) => {
-	const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleNavigation = (id?: string) => {
-		navigate(`/team/edit/${id ?? "new"}`);
-	};
+  const handleNavigation = (id?: string) => {
+    navigate(`/team/edit/${id ?? "new"}`);
+  };
 
-	return (
-		<div className="flex flex-col items-center">
-			<h1 className="w-full text-5xl font-serif text-center font-bold ">
-				Select a Team
-			</h1>
-			{/* Search bar */}
-			<div className="relative p-0 max-w-screen-md  w-full">
-				<input
-					type="text"
-					placeholder="Search..."
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-					className="text-CTA-500 
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="w-full text-5xl font-serif text-center font-bold ">
+        Select a Team
+      </h1>
+      {/* Search bar */}
+      <div className="relative p-0 max-w-screen-md  w-full">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="text-CTA-500 
 					border-2 border-CTA-500 
 					bg-CTA-100 w-full rounded-lg my-2 px-8 py-1 text-lg
 					placeholder:italic placeholder:text-CTA-300"
-				/>
-				<div className="absolute top-4 left-2">
-					<SearchLogo className="stroke-CTA-500 stroke-0 fill-CTA-500" />
-				</div>
-			</div>
+        />
+        <div className="absolute top-4 left-2">
+          <SearchLogo className="stroke-CTA-500 stroke-0 fill-CTA-500" />
+        </div>
+      </div>
 
-			<section className="max-w-screen-md  w-full">
-				{/* Add new team btn */}
-				<button
-					className="text-gray-800 border-dashed border-2 border-gray-800 p-8 bg-gray-200 w-full text-2xl font-bold hover:bg-gray-300 focus:bg-red-50 hover:border-solid transition-all ease-in"
-					onClick={(e) => handleNavigation()}
-				>
-					Add new team
-				</button>
-			</section>
-			{/* Teams */}
-			<section className="grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3  gap-5 my-3 w-full max-w-screen-md ">
-				{teams.map((t, idx) => (
-					<TeamDisplay team={t} key={idx} />
-				))}
-			</section>
-		</div>
-	);
+      <section className="max-w-screen-md  w-full">
+        {/* Add new team btn */}
+        <button
+          className="text-gray-800 border-dashed border-2 border-gray-800 p-8 bg-gray-200 w-full text-2xl font-bold hover:bg-gray-300 focus:bg-red-50 hover:border-solid transition-all ease-in"
+          onClick={(e) => handleNavigation()}
+        >
+          Add new team
+        </button>
+      </section>
+      {/* Teams */}
+      <section className="grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3  gap-5 my-3 w-full max-w-screen-md ">
+        {!query
+          ? teams.map((t, idx) => <TeamDisplay team={t} key={idx} />)
+          : teams.map((t, idx) => {
+              return t.name.toLowerCase().includes(query.toLowerCase()) ? (
+                <TeamDisplay team={t} key={idx} />
+              ) : null;
+            })}
+      </section>
+    </div>
+  );
 });
