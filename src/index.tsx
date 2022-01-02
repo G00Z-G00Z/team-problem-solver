01a8354305c9b9b00cec1a31996b95340b5133c4
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-02 08:45:41 -0600

    Changed Browser Router for HashRouter
    This makes so that the "Parent" url is not tampered
    
    Fixes #18

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
