e7eff5c7a392c8c4d0b07b7b9a642078290695fb
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-05 09:29:45 -0600

    Added shuffleArray method

---
+++ src/utils/shuffleArray.ts
@@ -0,0 +1,14 @@
+// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way,)%20%3D%3E%200.5%20%2D%20Math.
+/**
+ * Shuffles the array in place
+ * @param array Array
+ */
+export function shuffleArray<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
