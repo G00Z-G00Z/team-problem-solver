0c427e554e72c2148326084e55c072de26abdf87
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-15 20:33:03 -0600

    Modified task weight selector

--- src/ProblemPages/DivideUpWork/TaskWeightSelector.tsx
+++ src/ProblemPages/DivideUpWork/TaskWeightSelector.tsx
@@ -1,11 +1,24 @@
-import React, { FC } from 'react'
+import React, {
+  FC,
+  useEffect,
+  useRef,
+  useState
+  } from 'react'
 
 interface Props {
-  weight: number;
+  weight: number | string;
   onChange: (newWeight: string) => void;
 }
 
+const maxWeight = 5;
+const minWeight = 1;
+
+const isInRange = (n: number) => minWeight <= n && n < maxWeight + 1;
+
 export const TaskWeightSelector: FC<Props> = ({ weight, onChange }) => {
+  if (!isInRange(Number(weight)))
+    onChange(String(Number(weight) > maxWeight ? maxWeight : minWeight));
+
   return (
     <select
       value={weight}
