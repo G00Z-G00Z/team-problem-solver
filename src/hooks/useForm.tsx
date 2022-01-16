b01f3e014f2c66f41abedc220f7c0e0375d2cc63
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-15 19:55:30 -0600

    Added reset functionality

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react'
 //https://github.com/G00Z-G00Z/custom-hooks-react/tree/main/src/hooks/useForm
-import { useState } from "react";
 
 const useForm = <T extends Object>(initialState: T) => {
-	const [formulario, setFormulario] = useState<T>(initialState);
+  const [formulario, setFormulario] = useState<T>(initialState);
 
-	const onChange = (newValue: string, campo: keyof T) => {
-		setFormulario({
-			...formulario,
-			[campo]: newValue,
-		});
-	};
+  const onChange = (newValue: string, campo: keyof T) => {
+    setFormulario({
+      ...formulario,
+      [campo]: newValue,
+    });
+  };
 
-	return {
-		...formulario,
-		formulario,
-		onChange,
-	};
+  const reset = () => {
+    setFormulario(initialState);
+  };
+  return {
+    ...formulario,
+    formulario,
+    onChange,
+    reset,
+  };
 };
 
 export default useForm;
