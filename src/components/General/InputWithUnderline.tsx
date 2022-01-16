363b9120d4ca984592146a53c3e55c8011307ac7
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-16 11:03:41 -0600

    Made component input with underline

---
+++ src/components/General/InputWithUnderline.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react'
+import { appColors, AvailableColorNames } from '../../types/AppColors'
+
+interface Props {
+  value: string;
+  onChange: (v: string) => void;
+  onKeyDown?: (e: { key: string }) => void;
+  color?: AvailableColorNames;
+  ref?: React.RefObject<HTMLInputElement>;
+}
+
+export const InputWithUnderline: FC<Props> = ({
+  value,
+  onChange,
+  onKeyDown,
+  color = "gray",
+}) => {
+  return (
+    <div
+      className="relative group hover:border-0   h-10 
+          w-full max-w-md"
+    >
+      <input
+        type="text"
+        name="name"
+        autoComplete="off"
+        className="
+              text-xl text-center  
+             bg-transparent
+              decoration-gray-200 p-1 transition-all focus-visible:outline-0 
+              absolute top-1/2 left-1/2 transform -translate-x-1/2 
+              -translate-y-1/2
+              w-fit"
+        onChange={(e) => onChange(e.target.value)}
+        value={value}
+        onKeyDown={onKeyDown}
+      />
+
+      {/* underline active*/}
+      <div
+        style={{
+          backgroundColor: appColors[color][300],
+        }}
+        className="absolute w-0 group-hover:w-full h-[2px]  bottom-0 left-0 transition-all rounded-sm "
+      ></div>
+      <div className="absolute group-hover:w-0 w-full h-[2px] bg-gray-200 bottom-0 left-0  "></div>
+    </div>
+  );
+};
