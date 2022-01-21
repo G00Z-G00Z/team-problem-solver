9ae85176e710bdb571da1716f09cf3bc4b875ee6
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-21 17:44:21 -0600

    Changed backgorund for splash images

--- assetGenerator.sh
+++ assetGenerator.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # https://www.npmjs.com/package/pwa-asset-generator
 cd public/
-echo "Generando los assets a carpeta ./temp"
+echo "Generando los assets a carpeta public/png"
 npx pwa-asset-generator \
 ./favicon.ico \
 -m ./manifest.json  \
 -i ./index.html  \
 -type png \
--a "%PUBLIC_URL%"
+-a "%PUBLIC_URL%" \
+-b "#11946E" \
 cd ..
 
