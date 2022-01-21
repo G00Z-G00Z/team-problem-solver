eded2304640b1c04871818c1a3cdfa75d665fc85
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-21 17:06:38 -0600

    Making changes to make app compatible with ios

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import './styles/index.css'
+import './styles/mobileBehaviour.css'
 
 ReactDOM.render(
   <React.StrictMode>
