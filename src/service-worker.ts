a9c3e032a51bafdda3068dd45706d0dc5c90bd7d
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-24 16:58:59 -0600

    Made assets folder cache available

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,72 +1,31 @@
-import { clientsClaim } from 'workbox-core'
-import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { ExpirationPlugin } from 'workbox-expiration'
-import { registerRoute } from 'workbox-routing'
-import {
-  CacheFirst,
-  CacheOnly,
-  StaleWhileRevalidate,
-} from "workbox-strategies";
+import { CacheFirst, CacheOnly } from "workbox-strategies";
+import { clientsClaim } from "workbox-core";
+import { createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
+import { ExpirationPlugin } from "workbox-expiration";
+import { registerRoute } from "workbox-routing";
 /// <reference lib="webworker" />
 /* eslint-disable no-restricted-globals */
 
 declare const self: ServiceWorkerGlobalScope;
 
+const assetsCache = "assets-cache";
+const folderWithProblemPagesImages = "assets";
+
 clientsClaim();
 
-// Precaching
+// Precaching app shell
 precacheAndRoute(self.__WB_MANIFEST);
-precacheAndRoute([{ url: "/index.html" }, { url: "/favicon.ico" }]);
 
+// Assets cache
 registerRoute(
-  ({ request }) => {
-    return request.destination === "style";
-  },
-  new CacheOnly({
-    cacheName: "styles-cache",
+  ({ url }) =>
+    url.origin === self.location.origin &&
+    url.pathname.includes(`${folderWithProblemPagesImages}/`),
+
+  new CacheFirst({
+    cacheName: assetsCache,
   })
 );
-
-registerRoute(
-  ({ request }) => {
-    return request.url.endsWith(".js");
-  },
-  new CacheOnly({
-    cacheName: "javascript-cache",
-  })
-);
-
-// Set up App Shell-style routing, so that all navigation requests
-// are fulfilled with your index.html shell. Learn more at
-// https://developers.google.com/web/fundamentals/architecture/app-shell
-// const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
-// registerRoute(
-//   // Return false to exempt requests from being fulfilled by index.html.
-//   ({ request, url }: { request: Request; url: URL }) => {
-//     // If this isn't a navigation, skip.
-//     if (request.mode !== "navigate") {
-//       return false;
-//     }
-
-//     // If this is a URL that starts with /_, skip.
-//     if (url.pathname.startsWith("/_")) {
-//       return false;
-//     }
-
-//     // If this looks like a URL for a resource, because it contains
-//     // a file extension, skip.
-//     if (url.pathname.match(fileExtensionRegexp)) {
-//       return false;
-//     }
-
-//     // Return true to signal that we want to use the handler.
-//     return true;
-//   },
-//   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
-// );
-
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
@@ -75,5 +34,3 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
-
-// Any other custom service worker logic can go here.
