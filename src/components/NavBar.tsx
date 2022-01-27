0fce7617e53db0797c8ca59d3dd33057cbf61360
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-27 11:33:37 -0600

    Darkmode: Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,11 +9,21 @@ export const NavBar = () => {
   const constantNavStyle = "";
 
   function handleActiveClass({ isActive = false }) {
-    return `${constantNavStyle} ${isActive ? "fill-CTA-400" : "fill-gray-300"}`;
+    return `${constantNavStyle} ${
+      isActive
+        ? "fill-CTA-200 dark:fill-CTA-400"
+        : "fill-gray-300 dark:fill-gray-600"
+    }`;
   }
 
   return (
-    <nav className="w-full fixed bottom-0 border-b-2 z-10 border-t-solid border-t-[1px] border-t-gray-400 py-3 bg-gray-100 pb-6 sm:pb-6 md:pb-3 lg:pb-2">
+    <nav
+      className="w-full fixed bottom-0 border-b-2 z-10 border-t-solid border-t-[1px] border-t-gray-400 py-3 bg-gray-100 pb-6 sm:pb-6 md:pb-3 lg:pb-2
+    
+    dark:bg-gray-900 dark:border-t-gray-700
+    
+    "
+    >
       <ul className="flex justify-evenly">
         <li>
           <NavLink className={handleActiveClass} to="/">
