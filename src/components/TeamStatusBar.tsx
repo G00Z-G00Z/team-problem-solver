13df51531a045a88757db12742883639311890a8
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-27 11:18:55 -0600

    Darkmode: Status bar

--- src/components/TeamStatusBar.tsx
+++ src/components/TeamStatusBar.tsx
@@ -2,15 +2,18 @@ import React, { FC, useContext } from 'react'
 import { appColors } from '../types/AppColors'
 import { SelectedTeamContext } from '../context/SelectedTeamContext'
 import { Team } from '../types/interfaces'
+import { UiContext } from '../context/uiContext'
 
 interface Props {
   team: Team | undefined;
 }
 
 export const TeamStatusBar: FC<Props> = ({ team }) => {
+  const { darkmode } = useContext(UiContext);
+
   if (!team)
     return (
-      <div className="bg-gray-300 flex justify-evenly relative">
+      <div className="bg-gray-300 flex justify-evenly relative dark:bg-gray-700 dark:text-gray-200">
         Selecciona un equipo
       </div>
     );
@@ -20,10 +23,12 @@ export const TeamStatusBar: FC<Props> = ({ team }) => {
   return (
     <div
       style={{
-        backgroundColor: appColors[color][100],
+        backgroundColor: darkmode
+          ? appColors[color][200]
+          : appColors[color][100],
         color: appColors[color][500],
       }}
-      className="bg-CTA-100 flex justify-evenly relative font-bold"
+      className="flex justify-evenly relative font-bold"
     >
       {name}
     </div>
