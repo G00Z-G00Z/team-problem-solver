ece72de1ef9248b1dc8723159c150c37c1dfb791
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-27 12:49:53 -0600

    Darkmode: Help page

--- src/components/HelpPage.tsx
+++ src/components/HelpPage.tsx
@@ -13,33 +13,35 @@ export const Help = () => {
       <h1 className="text-gray-900 dark:text-gray-200 text-3xl font-serif text-center mb-2 ">
         What is this app ?
       </h1>
-      <p className="text-gray-800">
+      <p className="text-gray-800 dark:text-gray-300">
         This app has the purpose of{" "}
-        <strong className="text-CTA-500 ">solving problems</strong> that could
-        happend amongs teams. For example:
+        <strong className="text-CTA-500 dark:text-CTA-400">
+          solving problems
+        </strong>{" "}
+        that could happend amongs teams. For example:
       </p>
 
-      <ul className="flex flex-col gap-4">
-        <li className="bg-CTA-100 text-CTA-500 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
+      <ul className="flex flex-col gap-4 ">
+        <li className="bg-CTA-100 text-CTA-500 dark:bg-CTA-400 dark:text-CTA-100 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
           Whenever you are in a board game, and you must decide{" "}
-          <strong>which one goes first.</strong>
+          <strong className="">which one goes first.</strong>
         </li>
-        <li className="bg-gray-200 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
+        <li className="bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-300 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
           When you are dividing tasks for a team project, you can{" "}
           <strong>divide the tasks randomly</strong>, acording to ther weight in
           workload
         </li>
-        <li className="bg-CTA-100 text-CTA-500 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
+        <li className="bg-CTA-100 text-CTA-500 dark:bg-CTA-400 dark:text-CTA-100 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
           When you are deciding <strong>who's gonna go first</strong>.{" "}
         </li>
-        <li className="bg-gray-200 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
+        <li className="bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-300 p-4 rounded-md shadow-md hover:shadow-lg transition-all">
           When you try to decide something, you can{" "}
           <strong>choose a person randomly</strong>
         </li>
       </ul>
 
       <button
-        className="mt-4 bg-CTA-400 text-gray-100  h-8 px-3 text-xl rounded-md disabled:opacity-75 hover:bg-CTA-500 transition-all"
+        className="mt-4 bg-CTA-400 text-CTA-100  h-8 px-3 text-xl rounded-md disabled:opacity-75 hover:bg-CTA-500 transition-all"
         onClick={handleClick}
       >
         Go solve my problem!!
