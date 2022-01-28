65cf823b70e94a4cda0b40fb251ea5359262e902
G00Z_G00Z <edygomezgg4@gmail.com>
2022-01-28 16:38:18 -0600

    Darkmode: MemberWithJobs

--- src/ProblemPages/DivideUpWork/MemberWithJobsDisplay.tsx
+++ src/ProblemPages/DivideUpWork/MemberWithJobsDisplay.tsx
@@ -1,27 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { appColors, AvailableColorNames } from '../../types/AppColors'
 import { MemberWithJobs } from '../../problem-algorithms/divideUpWork'
+import { UiContext } from '../../context/uiContext'
 
 export const MemberWithJobsDisplay: FC<
   MemberWithJobs & { color: AvailableColorNames }
 > = ({ member, jobs, workload, color }) => {
+  const { darkmode } = useContext(UiContext);
+
   return (
     <div
       style={{
-        borderColor: appColors[color][400],
+        borderColor: darkmode ? appColors[color][100] : appColors[color][500],
       }}
       className="w-full border-2 border-dashed  max-w-sm"
     >
       <h2
         style={{
-          backgroundColor: appColors[color][100],
+          backgroundColor: darkmode
+            ? appColors[color][200]
+            : appColors[color][100],
           color: appColors[color][500],
         }}
         className="text-center font-bold "
       >
         {member.name}
       </h2>
-      <div className="p-4">
+      <div className="p-4 text-gray-800 dark:text-gray-200">
         {jobs.length > 0 ? (
           <>
             <p>
@@ -32,7 +37,7 @@ export const MemberWithJobsDisplay: FC<
               <b>Tasks: </b>
               <ul>
                 {jobs.map((job, idx) => (
-                  <li key={idx}>
+                  <li key={idx} className="">
                     {job.desc} : {job.weight}{" "}
                   </li>
                 ))}
@@ -40,7 +45,7 @@ export const MemberWithJobsDisplay: FC<
             </div>
           </>
         ) : (
-          <div className="w-full text-center">
+          <div className="w-full text-center text-gray-800 dark:text-gray-200">
             ¡Felicidades! No te tocó nada!
           </div>
         )}
